ef79d6c4dd51b47f3ab364d3fce54ed3ed8e4ac5
xlassix <awhy14539@gmail.com>
2025-11-20 01:17:00 +0100

    Remove console.log from fetch method

--- ts/src/base/Exchange.ts
+++ ts/src/base/Exchange.ts
@@ -3119,7 +3119,6 @@ export default class Exchange {
     // ! Usher Labs Addition: Modified to use an Axios-like override HTTP client if instantiated.
     async fetch(url, method = 'GET', headers: any = undefined, body: any = undefined) {
 
-        console.log({url, method, headers, body});
         // load node-http(s) modules only on first call
         if (isNode) {
             if (!this.nodeHttpModuleLoaded) {
